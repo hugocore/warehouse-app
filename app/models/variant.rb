018dbd04d5b04e05905e98d415e357227f6ce70e
Hugo Sequeira <hugoandresequeira@gmail.com>
2020-09-23 20:30:21 +0100

    Add validations to Variants

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -1,3 +1,6 @@
 class Variant < ApplicationRecord
   belongs_to :product
+
+  validates :sku, presence: true, uniqueness: true
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
