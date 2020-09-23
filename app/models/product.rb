0c8520e28a4cfc4f90f023284d058f95a58adcf9
Hugo Sequeira <hugoandresequeira@gmail.com>
2020-09-23 20:30:26 +0100

    Add validations to Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,11 @@
 class Product < ApplicationRecord
   has_many :variants, dependent: :destroy
 
+  MAX_DESCRIPTION_LENGTH = 300.freeze
+
+  validates :title, presence: true, uniqueness: true
+  validates :description, length: { maximum: MAX_DESCRIPTION_LENGTH }, allow_blank: true
+  validates :variants, presence: true
+
   accepts_nested_attributes_for :variants, allow_destroy: true
 end
