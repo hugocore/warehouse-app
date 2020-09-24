4a82902f1e17a774cc367ea0d2c51ecd0dbbc6d1
Hugo Sequeira <hugoandresequeira@gmail.com>
2020-09-24 12:09:41 +0100

    Scope uniqueness by soft-delete

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -1,6 +1,9 @@
 class Variant < ApplicationRecord
+  include Discard::Model
+
   belongs_to :product
 
-  validates :sku, presence: true, uniqueness: true
+  validates :sku, presence: true
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :sku, uniqueness: { conditions: -> { where(discarded_at: nil) } }
 end
